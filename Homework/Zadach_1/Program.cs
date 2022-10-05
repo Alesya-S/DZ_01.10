f6328577794921b31da8a52688edbb0c9e394c5a
Alesya <ales1981@mail.ru>
2022-10-05 21:54:59 +0300

    Zadacha_1

---
+++ Homework/Zadach_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max) max = b;
+
+ Console.WriteLine(max);
