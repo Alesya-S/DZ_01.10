eb612dd946aedccfb6e8b82f4677ab78dd77d2e9
Alesya <ales1981@mail.ru>
2022-10-06 00:47:00 +0300

    Izmenenie

--- Homework/Zadach_1/Program.cs
+++ Homework/Zadach_1/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите первое число");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+ какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
