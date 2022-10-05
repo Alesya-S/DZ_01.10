abd30aecc24467166c06f65ae73199ca1c12a9f7
Alesya <ales1981@mail.ru>
2022-10-06 00:44:19 +0300

    Zadacha_2

---
+++ Homework/Zadacha_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт
+ максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a<b) max = b;
+if (b<c) max = c;
+{
+    Console.WriteLine("Максимальное число равно "+max+ "" );
+}
+    
+
+
+// ile (n<100|n>999);}
+
+
+//Console.Write("Максимальная цифра числа:"+ Convert.ToString(n = max));
