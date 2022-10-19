2ad7f4ae1364751d03f19c09a8f3e35ab9421ac2
Alesya <ales1981@mail.ru>
2022-10-19 12:16:14 +0300

    Zadacha_30

--- Homework/Zadacha_30/Program.cs
+++ Homework/Zadacha_30/Program.cs
@@ -11,12 +11,25 @@ Console.Write("[");
 
 for (int i = 0; i < numbers.Length; i++)
 {
-    numbers [i] = new Random().Next(0,10);
-    Console.Write($"{i}"); 
+    numbers [i] = new Random().Next(0,9);
+    Console.Write(numbers[i]); 
     if (i < (numbers.Length-1)) Console.Write($", ");
 }
 Console.Write("]"); 
 
 
+/* int [] array = new int [8];
+int len = array.Length;
+int index = 0;
 
+while (index < len)
+{
+    array [index] = new Random().Next(0,10);
+    index ++;
+}
+for (int i = 0; i < len; i++)
+{
+    Console.Write(array[i]); 
+    if (i < (array.Length-1)) Console.Write($", ");
+} */
 
