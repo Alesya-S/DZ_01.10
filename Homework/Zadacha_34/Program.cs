98ef86de099f8a439330e1b63c38129cb8190873
Alesya <ales1981@mail.ru>
2022-10-21 14:03:26 +0300

    Zadacha_34

---
+++ Homework/Zadacha_34/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int [] array = new int [6];
+
+newArray (array);
+printArray(array);
+numFind(array);
+
+void newArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+      array [i] = new Random().Next(99, 1000);
+    }
+    return;
+}
+
+void printArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+void numFind(int[] array){
+    int result = 0;
+    {
+    for(int i = 0; i <array.Length; i++)
+       {
+        if (array[i]%2==0)
+        result++;
+       }
+        Console.WriteLine($"четных чисел в массиве {result} ");
+    }
+    Console.WriteLine();
+}
---
+++ Homework/Zagotovka/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+    int count = 0;
+    {
+    for(int i = 2; i <= count; i += 2)
+    {
+             Console.WriteLine(count);
+    }
+   //     Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
