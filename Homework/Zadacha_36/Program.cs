605699f32d6ff32e33cec4322e75a732e04daf3d
Alesya <ales1981@mail.ru>
2022-10-21 14:38:35 +0300

    Zadacha_36

---
+++ Homework/Zadacha_36/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int [] array = new int [6];
+
+newArray (array);
+printArray(array);
+numFind(array);
+
+void newArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+      array [i] = new Random().Next(0, 10);
+    }
+    return;
+}
+
+void printArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+void numFind(int[] array){
+    int sum = 0;
+    {
+    for(int i = 0; i <array.Length; i+=2)
+       {
+        sum = sum + array[i];
+       }
+        Console.WriteLine($"Сумма элементов на нечетных позициях равна {sum} ");
+    }
+    Console.WriteLine();
+}
--- Homework/Zagotovka/Program.cs
+++ Homework/Zagotovka/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-    int count = 0;
+﻿/*    int count = 0;
     {
     for(int i = 2; i <= count; i += 2)
     {
@@ -8,4 +6,40 @@
     }
    //     Console.Write(array[i]+" ");
     }
+    Console.WriteLine();*/
+
+
+    int [] array = new int [6];
+
+newArray (array);
+printArray(array);
+numFind(array);
+
+void newArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+      array [i] = new Random().Next(99, 1000);
+    }
+    return;
+}
+
+void printArray(int[] array){
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
     Console.WriteLine();
+}
+
+void numFind(int[] array){
+    int result = 0;
+    {
+    for(int i = 0; i <array.Length; i++)
+       {
+        if (array[i]%2==0)
+        result++;
+       }
+        Console.WriteLine($"четных чисел в массиве {result} ");
+    }
+    Console.WriteLine();
+}
