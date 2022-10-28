7388dfce3d8b2aebe90d764bb0f699adfcb41d33
Alesya <ales1981@mail.ru>
2022-10-28 11:46:46 +0300

    Zadacha_47

---
+++ Homework/Zadacha_47/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n,
+ заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.Write("Введите высоту матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длинну матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double [m, n];
+
+
+void FillArray(double [,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++){
+        for (int j = 0; j < matr.GetLength(1); j++){
+            matr [i,j] = new Random().Next(-10,10);
+        }
+    }
+}
+void Print(double [,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++){
+        for (int j = 0; j < matr.GetLength(1); j++){
+            Console.Write(matr[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+Print(array);
