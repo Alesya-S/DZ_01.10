4ebee3bcc2ff485258124fac808c3a3030f1cdff
Alesya <ales1981@mail.ru>
2022-11-30 14:52:11 +0300

    Zadacha_66

---
+++ Homework/Zadacha_66/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 66. Задайте значение M и N. Напишите программу, которая найдет сумму 
+натуральных элементов в промежутке от M д N.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30 */
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+    return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
